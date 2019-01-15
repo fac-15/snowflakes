1c1dae1b2a101f644a1783a687b898a2c9727dc5
Whooolia <tpdms2779@gmail.com>
2019-01-15 11:54:40 +0000

    Add test for database

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,6 +1,6 @@
 // const request = require('supertest');
 // const app = require('../server');
 
-// test('Jest is working', () => {
-//   expect(1 + 1).toBe(2);
-// });
+test('Jest is working', () => {
+  expect(1 + 1).toBe(2);
+});
