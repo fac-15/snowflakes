5a62d8ab773d6b5c300d728d8fe2e0cc1b0ae385
Cynthia Onwordi <cynthiaonwordi@Cynthias-MacBook-Pro.local>
2019-01-16 15:00:06 +0000

    removed comments & added home controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const error = require('./error');
+const home = require('./home')
 const topThree = require('./topthree');
 const explore = require('./explore');
 
-
+router.get('/', home.get);
 router.get('/topthree', topThree.get);
 router.get('/explore', explore.get);
 router.use(error.clients);
