e4c06e4d11324128321fb5e2b058594b7b8727c3
Kristina Jaggard <jaggardk@gmail.com>
2019-01-16 15:32:30 +0000

    amended database files with accurate schemas and test data

--- db/db_build.sql
+++ db/db_build.sql
@@ -49,37 +49,127 @@ INSERT INTO petitions (name, url, description) VALUES
 (
     'petition_one',
     'http://neopets.com',
-    'Only the best website in the history of the internet',
+    'Only the best website in the history of the internet'
+
+);
+
+INSERT INTO petitions (name, url, description) VALUES
+(
+    'petition_two',
+    'http://neopets.com',
+    'Only the best website in the history of the internet'
+
+);
+
+INSERT INTO petitions (name, url, description) VALUES
+(
+    'petition_three',
+    'http://neopets.com',
+    'Only the best website in the history of the internet'
 
 );
 
 INSERT INTO topthree (name, url, description) VALUES
 (
-  'topthree_data',
+  'topthree_data_1',
   'http://neopets.com',
   'Only the best website in the history of the internet'
 );
+
+INSERT INTO topthree (name, url, description) VALUES
+(
+  'topthree_data_2',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
+INSERT INTO topthree (name, url, description) VALUES
+(
+  'topthree_data_3',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
 INSERT INTO organisations (name, url, description) VALUES
 (
-  'organizations_data',
+  'organizations_data_1',
   'http://neopets.com',
   'Only the best website in the history of the internet'
 );
+
+INSERT INTO organisations (name, url, description) VALUES
+(
+  'organizations_data_2',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
+INSERT INTO organisations (name, url, description) VALUES
+(
+  'organizations_data_3',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
 INSERT INTO events (name, url, description) VALUES
 (
-  'events_data',
+  'events_data_1',
   'http://neopets.com',
   'Only the best website in the history of the internet'
 );
+
+INSERT INTO events (name, url, description) VALUES
+(
+  'events_data_2',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
+INSERT INTO events (name, url, description) VALUES
+(
+  'events_data_3',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
 INSERT INTO charities (name, url, description) VALUES
 (
-  'charities_data',
+  'charities_data_1',
   'http://neopets.com',
   'Only the best website in the history of the internet'
 );
+
+INSERT INTO charities (name, url, description) VALUES
+(
+  'charities_data_2',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
+INSERT INTO charities (name, url, description) VALUES
+(
+  'charities_data_3',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
 INSERT INTO followers (name, url, description) VALUES
 (
-  'followers_data',
+  'followers_data_1',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
+INSERT INTO followers (name, url, description) VALUES
+(
+  'followers_data_2',
+  'http://neopets.com',
+  'Only the best website in the history of the internet'
+);
+
+INSERT INTO followers (name, url, description) VALUES
+(
+  'followers_data_3',
   'http://neopets.com',
   'Only the best website in the history of the internet'
 );
--- db/test_schema.sql
+++ db/test_schema.sql
@@ -2,47 +2,47 @@ BEGIN;
 
 CREATE TABLE topthree (
     id SERIAL PRIMARY KEY,
-    top_one VARCHAR(200) NOT NULL,
-    top_two VARCHAR(200) NOT NULL,
-    top_three VARCHAR(200) NOT NULL
+    name VARCHAR(200) NOT NULL,
+    url VARCHAR(200) NOT NULL,
+    description VARCHAR(500)
 );
 
 CREATE TABLE organisations (
     id SERIAL PRIMARY KEY,
-    org_one VARCHAR(200) NOT NULL,
-    org_two VARCHAR(200) NOT NULL,
-    org_three VARCHAR(200) NOT NULL
+    name VARCHAR(200) NOT NULL,
+    url VARCHAR(200) NOT NULL,
+    description VARCHAR(500)
 );
 
 
 CREATE TABLE events (
     id SERIAL PRIMARY KEY,
-    event_one VARCHAR(200) NOT NULL,
-    event_two VARCHAR(200) NOT NULL,
-    event_three VARCHAR(200) NOT NULL
+    name VARCHAR(200) NOT NULL,
+    url VARCHAR(200) NOT NULL,
+    description VARCHAR(500)
 );
 
 
 CREATE TABLE charities (
     id SERIAL PRIMARY KEY,
-    charity_one VARCHAR(200) NOT NULL,
-    charity_two VARCHAR(200) NOT NULL,
-    charity_three VARCHAR(200) NOT NULL
+    name VARCHAR(200) NOT NULL,
+    url VARCHAR(200) NOT NULL,
+    description VARCHAR(500)
 );
 
 
 CREATE TABLE petitions (
     id SERIAL PRIMARY KEY,
-    petition_one VARCHAR(200) NOT NULL,
-    petition_two VARCHAR(200) NOT NULL,
-    petition_three VARCHAR(200) NOT NULL
+    name VARCHAR(200) NOT NULL,
+    url VARCHAR(200) NOT NULL,
+    description VARCHAR(500)
 );
 
 CREATE TABLE followers (
     id SERIAL PRIMARY KEY,
-    follower_one VARCHAR(200) NOT NULL,
-    follower_two VARCHAR(200) NOT NULL,
-    followers_three VARCHAR(200) NOT NULL
+    name VARCHAR(200) NOT NULL,
+    url VARCHAR(200) NOT NULL,
+    description VARCHAR(500)
 );
 
 COMMIT;
