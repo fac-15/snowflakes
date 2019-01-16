c1a5d9efa97288194ca0de4ccd5b73bb24294303
Kristina Jaggard <jaggardk@gmail.com>
2019-01-16 12:37:05 +0000

    adjustments to modal files

--- src/modal/getCharities.js
+++ src/modal/getCharities.js
@@ -1,5 +1,15 @@
 const dbConnection = require('../../db/db_connection.js');
 
-const getCharities = () =>
+const getCharities = (cb) => {
+    dbConnection.query('SELECT * from charities', (err, res) => {
+        if (err) {
+            console.log(`You have an error fetching data from Charities table: ${err}`);
+            return cb(err);
+        } else {
+            console.log(`Result: ${res.rows}`)
+            return cb(null, res.rows);
+        }
+    });
+};
 
-module.exports = 
+module.exports = getCharities;
--- src/modal/index.js
+++ src/modal/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-    getCharities: require('./getCharities'),
-    getEvents: require('./getEvents'),
-    getOrgs: require('./getOrgs'),
-    getPetitions: require('./getPetitions'),
-    getSocials: require('./getSocials')
+    getCharities: require('./getCharities')
+    // getEvents: require('./getEvents'),
+    // getOrgs: require('./getOrgs'),
+    // getPetitions: require('./getPetitions'),
+    // getSocials: require('./getSocials')
 };
