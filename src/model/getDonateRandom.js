eb6948b20142845242b381cee59552a6dc8e0cf8
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-01-17 17:20:21 +0000

    Add db call to randomly choose 1 row from the donate table

---
+++ src/model/getDonateRandom.js
@@ -0,0 +1,20 @@
+const dbConnection = require('./db/db_connection.js');
+
+const getDonateRandom = cb => {
+    dbConnection.query(
+        //selects a single random row from the database in order to populate the Top Three page
+        'SELECT * from donate ORDER BY RANDOM() LIMIT 1',
+        (err, res) => {
+            if (err) {
+                console.log(
+                    `You have an error fetching data from Donate table: ${err}`
+                );
+                return cb(err);
+            } else {
+                return cb(null, res.rows);
+            }
+        }
+    );
+};
+
+module.exports = getDonateRandom;
