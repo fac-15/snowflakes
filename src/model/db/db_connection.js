14f7236e21c69f9914d2b104b4ceb35ce6671b82
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-01-17 10:56:07 +0000

    Move all database files to the src folder within the model folder. Change relevant paths

--- src/model/db/db_connection.js
+++ src/model/db/db_connection.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const url = require('url');
 
 const env = require('env2');
-env('../../config.env');
+env('config.env');
 
 let DB_URL = process.env.DB_URL;
 if (process.env.NODE_ENV === 'test') {
