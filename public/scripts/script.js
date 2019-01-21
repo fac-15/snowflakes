245c657f0f68374f0591a7fda359e84a6f865a0a
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-01-21 15:49:26 +0000

    Make all categories display as carousels

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,31 +1,34 @@
 const carousel = function() {
-    const box = document.querySelector('.explore-carousel');
-    const next = box.querySelector('.next');
-    const prev = box.querySelector('.prev');
-    const items = box.querySelectorAll('.carousel-content li');
-    let counter = 0;
-    let amount = items.length;
-    let current = items[0];
-    box.classList.add('active');
-    function navigate(direction) {
-        current.classList.remove('current');
-        counter = counter + direction;
-        if (direction === -1 && counter < 0) {
-            counter = amount - 1;
+    const boxes = document.querySelectorAll('.explore-carousel');
+    boxes.forEach(box => {
+        console.log(box);
+        const next = box.querySelector('.next');
+        const prev = box.querySelector('.prev');
+        const items = box.querySelectorAll('.carousel-content li');
+        let counter = 0;
+        let amount = items.length;
+        let current = items[0];
+        box.classList.add('active');
+        function navigate(direction) {
+            current.classList.remove('current');
+            counter = counter + direction;
+            if (direction === -1 && counter < 0) {
+                counter = amount - 1;
+            }
+            if (direction === 1 && !items[counter]) {
+                counter = 0;
+            }
+            current = items[counter];
+            current.classList.add('current');
         }
-        if (direction === 1 && !items[counter]) {
-            counter = 0;
-        }
-        current = items[counter];
-        current.classList.add('current');
-    }
-    next.addEventListener('click', function(ev) {
-        navigate(1);
+        next.addEventListener('click', function(ev) {
+            navigate(1);
+        });
+        prev.addEventListener('click', function(ev) {
+            navigate(-1);
+        });
+        navigate(0);
     });
-    prev.addEventListener('click', function(ev) {
-        navigate(-1);
-    });
-    navigate(0);
 };
 
 carousel();
