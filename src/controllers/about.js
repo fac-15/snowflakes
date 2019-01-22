0a1f8206cf73942896852b9e5026899be97b5b33
Whooolia <tpdms2779@gmail.com>
2019-01-22 17:53:37 +0000

    Create about page

---
+++ src/controllers/about.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+exports.get = (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'about.hbs'));
+};
+
+exports.get = (req, res) => {
+    res.render('about');
+}
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,10 +5,12 @@ const error = require('./error');
 const home = require('./home')
 const topThree = require('./topthree');
 const explore = require('./explore');
+const about = require('./about');
 
 router.get('/', home.get);
 router.get('/topthree', topThree.get);
 router.get('/explore', explore.get);
+router.get('/about', about.get);
 router.use(error.clients);
 router.use(error.server);
 
