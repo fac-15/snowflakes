5691bc62e438dc4d60d4c0283af4c4e277a2c13b
Kristina Jaggard <37771591+teenie-quaggard@users.noreply.github.com>
2019-01-22 11:22:05 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.disable('x-powered-by');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.set('port', process.env.PORT || 4040);
+app.set('port', process.env.PORT || 4000);
 
 app.use(
     favicon(path.join(__dirname, '..', 'public', 'img/favicons/favicon.ico'))
